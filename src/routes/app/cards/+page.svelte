33b6cde94440326a638d0630c15b88ecaa319176
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-01-26 14:31:07 +0100

    Add plus button

--- src/routes/app/cards/+page.svelte
+++ src/routes/app/cards/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import { Icon } from '@steeze-ui/svelte-icon';
+  import { Plus } from '@steeze-ui/heroicons';
+
   import { titleStore } from '$lib/state';
 
   titleStore.set('Cards');
@@ -6,7 +9,7 @@
   export let data;
 </script>
 
-<div class="grid grid-cols-2 gap-2">
+<div class="grid grid-cols-1 gap-2">
   {#each data.cardAndCardTypes as cardAndCardType}
     <a href="/app/cards/{cardAndCardType.card.id}">
       <div
@@ -18,4 +21,10 @@
       </div>
     </a>
   {/each}
+
+  <a href="/app/cards/new">
+    <div class="flex h-[6em] w-full items-center justify-center rounded-md bg-red-50 text-red-800">
+      <Icon src={Plus} class="h-6 w-6" />
+    </div>
+  </a>
 </div>
