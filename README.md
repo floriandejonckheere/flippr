bb43e6e2a5304ec7088eaf7e21b1f3315b39e57c
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-01-26 15:39:36 +0100

    Add database seeds

--- README.md
+++ README.md
@@ -1,38 +1,44 @@
-# sv
+# Flippr
 
-Everything you need to build a Svelte project, powered by [`sv`](https://github.com/sveltejs/cli).
+Flippr is a simple, no-frills, no-ads loyalty card app.
+It's built with [SvelteKit](https://kit.svelte.dev/), [TailwindCSS](https://tailwindcss.com/), and [Heroicons](https://heroicons.com/).
 
-## Creating a project
+# Set up
 
-If you're seeing this, you've probably already done this step. Congrats!
+Create an `.env` file in the root of the project with the following content:
 
-```bash
-# create a new project in the current directory
-npx sv create
-
-# create a new project in my-app
-npx sv create my-app
+```
+DATABASE_URL="postgres://postgres:postgres@localhost:5432/flippr"
 ```
 
-## Developing
+Create a database:
 
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
+```
+docker compose up -d
+docker compose exec postgres -U postgres createdb flippr
+```
 
-```bash
+Install dependencies:
+
+```
+npm install
+```
+
+Seed the database (optional):
+
+```
+npm run db:seed
+```
+
+Run the app:
+
+```
 npm run dev
-
-# or start the server and open the app in a new browser tab
-npm run dev -- --open
 ```
 
-## Building
+Open [localhost:5173](http://localhost:5173) in your browser.
+If you seeded the database, you can log in with `admin`/`password` or `user`/`password`.
 
-To create a production version of your app:
+# License
 
-```bash
-npm run build
-```
-
-You can preview the production build with `npm run preview`.
-
-> To deploy your app, you may need to install an [adapter](https://svelte.dev/docs/kit/adapters) for your target environment.
+The software is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
---
+++ src/lib/server/db/seeds.ts
@@ -0,0 +1,150 @@
+import { hash } from '@node-rs/argon2';
+
+import { drizzle } from 'drizzle-orm/postgres-js';
+import * as dotenv from 'dotenv';
+
+import { v4 as uuidv4 } from 'uuid';
+
+import { users, cards, cardTypes } from './schema';
+import type { User, Card, CardType } from './schema';
+import postgres from 'postgres';
+
+dotenv.config({ path: './.env' });
+
+if (!('DATABASE_URL' in process.env)) throw new Error('DATABASE_URL not found in .env');
+
+const passwordHash = async (password: string) => {
+  return await hash(password, {
+    memoryCost: 19456,
+    timeCost: 2,
+    outputLen: 32,
+    parallelism: 1
+  });
+};
+
+const cardTypesData: { [key: string]: CardType } = {
+  delhaize: {
+    id: uuidv4(),
+    name: 'Delhaize',
+    backgroundColor: '#C8102E',
+    textColor: '#FFFFFF',
+    createdAt: new Date(Date.now())
+  },
+  colruyt: {
+    id: uuidv4(),
+    name: 'Colruyt',
+    backgroundColor: '#E8762A',
+    textColor: '#FFFFFF',
+    createdAt: new Date(Date.now())
+  },
+  carrefour: {
+    id: uuidv4(),
+    name: 'Carrefour',
+    backgroundColor: '#0058A6',
+    textColor: '#FFFFFF',
+    createdAt: new Date(Date.now())
+  },
+  aldi: {
+    id: uuidv4(),
+    name: 'Aldi',
+    backgroundColor: '#0094D1',
+    textColor: '#FFFFFF',
+    createdAt: new Date(Date.now())
+  },
+  lidl: {
+    id: uuidv4(),
+    name: 'Lidl',
+    backgroundColor: '#224EA2',
+    textColor: '#FFFFFF',
+    createdAt: new Date(Date.now())
+  }
+};
+
+const usersData: { [key: string]: User } = {
+  admin: {
+    id: uuidv4(),
+    email: 'admin@admin.org',
+    name: 'Administrator',
+    username: 'admin',
+    passwordHash: await passwordHash('password'),
+    createdAt: new Date(Date.now())
+  },
+  user: {
+    id: uuidv4(),
+    email: 'user@user.org',
+    name: 'User',
+    username: 'user',
+    passwordHash: await passwordHash('password'),
+    createdAt: new Date(Date.now())
+  }
+};
+
+const cardData: { [key: string]: { [key: string]: Card } } = {
+  admin: {
+    delhaize: {
+      id: uuidv4(),
+      userId: usersData.admin.id,
+      cardTypeId: cardTypesData.delhaize.id,
+      value: '1234 5678 9012 3456',
+      createdAt: new Date(Date.now())
+    },
+    colruyt: {
+      id: uuidv4(),
+      userId: usersData.admin.id,
+      cardTypeId: cardTypesData.colruyt.id,
+      value: '1234 5678 9012 3456',
+      createdAt: new Date(Date.now())
+    },
+    carrefour: {
+      id: uuidv4(),
+      userId: usersData.admin.id,
+      cardTypeId: cardTypesData.carrefour.id,
+      value: '1234 5678 9012 3456',
+      createdAt: new Date(Date.now())
+    }
+  },
+  user: {
+    delhaize: {
+      id: uuidv4(),
+      userId: usersData.user.id,
+      cardTypeId: cardTypesData.delhaize.id,
+      value: '1234 5678 9012 3456',
+      createdAt: new Date(Date.now())
+    },
+    aldi: {
+      id: uuidv4(),
+      userId: usersData.user.id,
+      cardTypeId: cardTypesData.aldi.id,
+      value: '1234 5678 9012 3456',
+      createdAt: new Date(Date.now())
+    },
+    lidl: {
+      id: uuidv4(),
+      userId: usersData.user.id,
+      cardTypeId: cardTypesData.lidl.id,
+      value: '1234 5678 9012 3456',
+      createdAt: new Date(Date.now())
+    }
+  }
+};
+
+const main = async () => {
+  // @ts-ignore
+  const client = postgres(process.env.DATABASE_URL);
+  const db = drizzle(client);
+
+  console.log('Seeding users...');
+  await db.insert(users).values(Object.values(usersData));
+
+  console.log('Seeding card types...');
+  await db.insert(cardTypes).values(Object.values(cardTypesData));
+
+  console.log('Seeding cards...');
+  for (const key of Object.keys(cardData)) {
+    await db.insert(cards).values(Object.values(cardData[key]));
+  }
+
+  console.log('Done!');
+};
+
+await main();
