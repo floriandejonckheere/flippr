e15f15aeb80af4bfed61526ced3e72ef8600dac9
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-02-02 13:51:26 +0100

    Allow mDNS hostname in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    allowedHosts: ['localhost', '127.0.0.1', 'azathoth.local'],
     fs: {
       allow: ['./static/uploads']
     }
