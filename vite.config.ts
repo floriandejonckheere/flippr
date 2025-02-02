5b80df6e006944be2dea2df680f67c3d966f309b
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-02-02 18:13:59 +0100

    Enable TLS on development server

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), mkcert()],
   server: {
     host: '0.0.0.0',
     port: 5173,
