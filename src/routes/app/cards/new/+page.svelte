b73ff602755d159c6e8ca5f7e31ea1e9608f10bc
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-03-17 15:41:27 +0100

    Change new card to two-step form

--- src/routes/app/cards/new/+page.svelte
+++ src/routes/app/cards/new/+page.svelte
@@ -1,73 +1,24 @@
 <script lang="ts">
-  import QrCodeScanner from '$lib/components/QrCodeScanner.svelte';
+  import type { ActionData } from './$types';
+
+  import Form from './Form.svelte';
 
   import { titleStore } from '$lib/state';
 
-  import { enhance } from '$app/forms';
-  import type { ActionData } from './$types';
+  titleStore.set('New card');
 
   let { form, data }: { form: ActionData; data: any } = $props();
-  let value = $state('');
 
-  titleStore.set('New card');
+  let active: number = $state(0);
+
+  const stepTo = (step: number) => {
+    active += step
+  }
 </script>
 
-<form method="POST" use:enhance>
-  <div class="grid gap-y-4">
-    {#if form?.message}
-      <p class="text-sm font-bold text-red-600">
-        Error: {form.message}
-      </p>
-    {/if}
-
-    <div>
-      <QrCodeScanner
-        scanSuccess={(e: any) => {
-          console.log(e);
-
-          value = e;
-        }}
-        scanFailure={(e: any) => {
-          // console.log(e);
-        }}
-        paused={false}
-        width={480}
-        height={480}
-        class="w-full max-w-sm overflow-hidden rounded-lg bg-slate-700"
-      />
-    </div>
-
-    <div>
-      <label for="cardTypeId" class="mb-2 block text-sm dark:text-white"> Type </label>
-      <select
-        id="cardTypeId"
-        name="cardTypeId"
-        class="block w-full rounded-lg border-gray-200 px-4 py-3 text-sm focus:border-sky-700 focus:ring-sky-700 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-700 dark:bg-slate-900 dark:text-gray-400 dark:focus:ring-gray-600"
-        required
-      >
-        {#each data.cardTypes as cardType}
-          <option value={cardType.id}>{cardType.name}</option>
-        {/each}
-      </select>
-    </div>
-
-    <div>
-      <label for="name" class="mb-2 block text-sm dark:text-white"> Value </label>
-      <input
-        type="text"
-        id="value"
-        name="value"
-        bind:value
-        class="block w-full rounded-lg border-gray-200 px-4 py-3 text-sm focus:border-sky-700 focus:ring-sky-700 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-700 dark:bg-slate-900 dark:text-gray-400 dark:focus:ring-gray-600"
-        required
-      />
-    </div>
-
-    <button
-      type="submit"
-      class="mt-4 inline-flex w-full items-center justify-center gap-x-2 rounded-lg border border-transparent bg-red-800 px-4 py-3 text-sm font-semibold text-white hover:bg-red-900 disabled:pointer-events-none disabled:opacity-50 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-    >
-      Save
-    </button>
-  </div>
-</form>
+<Form
+    form={form}
+    data={data}
+    step={active}
+    stepTo={stepTo}
+/>
