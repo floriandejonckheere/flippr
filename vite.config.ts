e924e448028a3626092415267a6e57d77c90083b
Florian Dejonckheere <florian@floriandejonckheere.be>
2025-03-17 14:15:09 +0100

    Store development certificates locally

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,18 @@ import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  plugins: [sveltekit(), mkcert()],
+  plugins: [sveltekit(), mkcert({
+    hosts: ['azathoth.local', 'nyarlathotep.local'],
+  })],
   server: {
     host: '0.0.0.0',
     port: 5173,
     allowedHosts: ['localhost', '127.0.0.1', 'azathoth.local'],
     fs: {
-      allow: ['./static/uploads']
+      allow: [
+        './static/uploads',
+        '/root/.vite-plugin-mkcert'
+      ]
     }
   }
 });
